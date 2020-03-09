8e2f81e3bfba2c33cc3225f15394a7c6467a387f
Kleber <krodrigo@gmail.com>
2020-03-09 19:33:03 -0300

    Formatar CPF

--- src/app/directives/cpf-format.directive.ts
+++ src/app/directives/cpf-format.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, AfterViewInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { FormattedCpfPipe } from '../pipes/formatted-cpf.pipe';
 
 @Directive({
   selector: '[appCpfFormat]'
 })
-export class CpfFormatDirective {
+export class CpfFormatDirective implements AfterViewInit {
+  private key: string;
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
-  constructor() { }
+  @HostListener('input', ['$event.target'])
+  onChange({ value }: { value: string}) {
+    if(!this.key || this.key !== 'Backspace') {
+      if(value.length == 11) {
+        this.element.nativeElement.value = new FormattedCpfPipe().transform(this.element.nativeElement.value);
+      }
+    }
+  }
 
+  @HostListener('keydown', ['$event'])
+  onKeyPress({ key}: {key: string}) {
+    this.key = key;
+  }
+
+  ngAfterViewInit() {
+    this.renderer.setAttribute(this.element.nativeElement, 'maxlength', '11');
+  }
 }
