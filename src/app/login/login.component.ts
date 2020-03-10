0f7ca62d909975a760865bb5ab5bbc540a2f4ed3
Benevides <beny1000@hotmail.com>
2020-03-10 19:02:03 -0300

    login implementing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { UserService } from "../services/client.service";
+import { AuthService } from "../services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup = this.fb.group({
+    password: this.fb.control("", Validators.required),
+    username: this.fb.control("", Validators.required)
+  });
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService
+        .authenticate(this.form.value)
+        .subscribe(() => this.router.navigateByUrl("/clientes"));
+    }
   }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,28 +1,35 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { User } from '../models/client';
-import { environment } from 'src/environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { User } from "../models/client";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  constructor(private Http: HttpClient) {}
 
-  constructor(private Http: HttpClient) { }
-
-  authenticate(username: string, password: string): Observable<User> {
-    return this.Http.post<User>(`${environment.api}/users/authenticate`,
-      {password, username}).pipe(tap(user => this.token = user.token));
+  authenticate({
+    username,
+    password
+  }: {
+    username: string;
+    password: string;
+  }): Observable<User> {
+    return this.Http.post<User>(`${environment.api}/users/authenticate`, {
+      password,
+      username
+    }).pipe(tap(user => (this.token = user.token)));
   }
 
   set token(token: string) {
-    localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
   }
 
   get token(): string {
-    return localStorage.getItem('token');
+    return localStorage.getItem("token");
   }
 
   get isLoggedIn(): boolean {
